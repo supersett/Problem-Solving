a9c800215ce1cd106aab2615098f9629035b155d
supersett <nolang3748@naver.com>
2022-05-03 18:59:13 +0900

    submit

---
+++ 4.일차원 배열/1546.py
@@ -0,0 +1,11 @@
+import sys
+n=int(sys.stdin.readline())
+sum=0
+data=list(map(int,sys.stdin.readline().split()))
+b=max(data)
+for y in range(0,len(data)):
+    data[y]=int((data[y]*100)/b)
+for y in range(0,len(data)):
+    sum+=data[y]
+average=sum/n
+print(average)
