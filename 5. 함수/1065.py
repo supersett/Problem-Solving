ec2c73cb331b1934c1994c196a25ff02b2589278
supersett <nolang3748@naver.com>
2022-05-08 19:24:01 +0900

    submit

---
+++ 5. 함수/1065.py
@@ -0,0 +1,15 @@
+import sys
+n=int(sys.stdin.readline())
+count=0
+for i in range(1,n+1):
+    #102
+    data=list(str(i))
+    #['1' '0' '2']
+    if len(data)==3:
+        if (int(data[1])-int(data[0]))==(int(data[2])-int(data[1])):
+            count+=1
+    elif len(data)==4:
+        break
+    else:
+        count+=1
+print(count)
