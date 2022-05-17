fc27e62a6239c0cf490638f6d84894db5d9e7d02
supersett <nolang3748@naver.com>
2022-05-17 14:10:04 +0900

    submit

--- 9. 브루트 포스/2798.py
+++ 9. 브루트 포스/2798.py
@@ -1,4 +1,10 @@
 import sys
 n,m=map(int,sys.stdin.readline().split())
 data=list(map(int,sys.stdin.readline().split()))
-
+sum=0
+for x in range(0,len(data)-2):
+    y=data[x]+data[x+1]+data[x+2]
+    if y>sum and y<=m:
+        sum=y
+        print(x)
+print(sum)
