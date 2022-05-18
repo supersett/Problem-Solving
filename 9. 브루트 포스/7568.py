d57fb4ea22c4af1f0c4b5596d8233faea8f42be7
supersett <nolang3748@naver.com>
2022-05-18 16:51:43 +0900

    submit

---
+++ 9. 브루트 포스/7568.py
@@ -0,0 +1,15 @@
+import sys
+num_student=int(sys.stdin.readline())
+student_list=[]
+
+for x in range(num_student):
+    weight,height=map(int,sys.stdin.readline().split())
+    student_list.append((weight,height))
+
+for i in student_list:
+    rank =1
+    for j in student_list:
+        if i[0] <j[0] and i[1]<j[1]:
+            rank+=1
+    print(rank,end=" ")
+
