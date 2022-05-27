b548d01034a8bdd9aa944e315cfd3a9014cfca40
supersett <nolang3748@naver.com>
2022-05-28 07:54:43 +0900

    submit

---
+++ 11. 정렬/1427.py
@@ -0,0 +1,8 @@
+import sys
+
+n=sys.stdin.readline().strip()
+target=list(map(int,list(n)))
+target.sort(reverse=True)
+target2=list(map(str,target))
+target3=''.join(target2)
+print(target3)
---
+++ 11. 정렬/2108.py
@@ -0,0 +1,26 @@
+import sys
+from collections import Counter
+import statistics
+
+n=int(sys.stdin.readline())
+data=[]
+for x in range(n):
+    data.append(int(sys.stdin.readline()))
+
+data.sort()
+
+cnt=Counter(data).most_common(2)
+
+print(round(sum(data)/n))
+print(statistics.median(data))
+
+if len(data)>1:
+    if cnt[0][1] == cnt[1][1]:
+        print(cnt[1][0])
+    else:
+        print(cnt[0][0])
+else:
+    print(cnt[0][0])
+        
+
+print(max(data)-min(data))
