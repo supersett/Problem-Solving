bc28e01b75c384e1769a8d67cfdbbe22bc89636d
supersett <nolang3748@naver.com>
2022-09-09 18:35:29 +0900

    fix code

--- 스터디/코테대비/[카카오인턴]수식최대화.py
+++ 스터디/코테대비/[카카오인턴]수식최대화.py
@@ -7,40 +7,40 @@ string_listtt=list(string_input)
 tq=string_listtt
 
 def calculate_bracket(string_given):
-    try:
-        count=string_given.count("(")
-        calculate_list_index=[]
-        #print(count)
-        what_we_want=string_given
-        for i in range(0,count):
-            #print(i)
-            for x in range(0,len(what_we_want)):
-                if what_we_want[x]=="(":
-                    calculate_list_index.append(x)
-                if what_we_want[x]==")":
-                    calculate_list_index.append(x)
-                    #print(calculate_list_index)
-                    #괄호로 묶인부분 합해주는 부분
-                    lista=what_we_want[(calculate_list_index[0]+1):(calculate_list_index[1])]
-                    #print(lista)
-                    s=''.join(lista)
-                    #print(s)
-                    data=eval(s)
-                    #print(data)
-                    data_string=str(data)
-                    #(500-100) -> 400 으로 바꿔주는 부분
-                    target=''.join(what_we_want[calculate_list_index[0]:calculate_list_index[1]+1])
-                    #print(target)
-                    sumver_string_given=''.join(what_we_want)
-                    sumver_string_given=sumver_string_given.replace(target,data_string)
-                    target_list=list(sumver_string_given)
-                    what_we_want=target_list
-                    #print(target_list)
-                    calculate_list_index.clear()
-                    break
-        #print(what_we_want)
-        return what_we_want
-    except SyntaxError:
+
+    count=string_given.count("(")
+    calculate_list_index=[]
+    #print(count)
+    what_we_want=string_given
+    for i in range(0,count):
+        #print(i)
+        for x in range(0,len(what_we_want)):
+            if what_we_want[x]=="(":
+                calculate_list_index.append(x)
+            if what_we_want[x]==")":
+                calculate_list_index.append(x)
+                #print(calculate_list_index)
+                #괄호로 묶인부분 합해주는 부분
+                lista=what_we_want[(calculate_list_index[0]+1):(calculate_list_index[1])]
+                #print(lista)
+                s=''.join(lista)
+                #print(s)
+                data=eval(s)
+                #print(data)
+                data_string=str(data)
+                #(500-100) -> 400 으로 바꿔주는 부분
+                target=''.join(what_we_want[calculate_list_index[0]:calculate_list_index[1]+1])
+                #print(target)
+                sumver_string_given=''.join(what_we_want)
+                sumver_string_given=sumver_string_given.replace(target,data_string)
+                target_list=list(sumver_string_given)
+                what_we_want=target_list
+                #print(target_list)
+                calculate_list_index.clear()
+                break
+    #print(what_we_want)
+    return what_we_want
+
         
 
 def make_bracket(string_list,keyword):
