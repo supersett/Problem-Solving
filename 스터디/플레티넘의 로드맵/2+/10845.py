0c06da155e8d4d1cb110694b7099d2ffb8c3a4bf
supersett <nolang3748@naver.com>
2022-10-06 16:57:40 +0900

    BOJ : 10845

---
+++ 스터디/플레티넘의 로드맵/2+/10845.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+
+n=int(input())
+
+st=deque([])
+an=[]
+for _ in range(n):
+    k=sys.stdin.readline().rstrip()
+    if "push" in k:
+        n,m=k.split()
+        st.append(m)
+    elif k=="pop":
+        #if not st
+        if len(st)==0:
+            an.append(-1)
+        else:
+            an.append(st.popleft())
+    elif k=="size":
+        an.append(len(st))
+    elif k=="empty":
+        if not st:
+            an.append(1)
+        else:
+            an.append(0)
+    elif k=="front":
+        if not st:
+            an.append(-1)
+        else:
+            an.append(st[0])
+    elif k=="back":
+        if not st:
+            an.append(-1)
+        else:
+            an.append(st[-1])        
+
+for _ in an:
+    print(_)
