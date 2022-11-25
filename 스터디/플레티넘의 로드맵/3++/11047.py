9aa7a665404d37fcba5d66e1d38cf0ed2c92e184
supersett <nolang3748@naver.com>
2022-11-25 20:32:29 +0900

    BOJ : 11047

---
+++ 스터디/플레티넘의 로드맵/3++/11047.py
@@ -0,0 +1,16 @@
+import sys
+target=[]
+answer=0
+tmp=0
+N,K=map(int,sys.stdin.readline().split())
+for _ in range(N):
+  target.append(int(sys.stdin.readline().rstrip()))
+
+target.sort(reverse=True)
+print(target)
+
+for i in target:
+  if K>=i:
+    answer+=K//i
+    K=K%i
+print(answer)
