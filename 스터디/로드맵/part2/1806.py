4e8c611771bfff0b7c40e95f9fae434f528fdd3c
supersett <nolang3748@naver.com>
2023-02-14 12:31:47 +0900

    BOJ : 1806 해설 추가

--- 스터디/로드맵/part2/1806.py
+++ 스터디/로드맵/part2/1806.py
@@ -18,6 +18,16 @@ while switch:
   
   if tmp>=s:
     min_length=min(min_length,point_back-point_front)
+    
+    #문제 접근 로직은 맞았는데 경계조건에서 반례 뚜들겨 맞으면서 시간이 오래걸림
+    #현재 자신 위치를 먼저 연산을 해야지.
+    #더하고나서 연산을 해버리면 point_front 0은 영영 계산을 못해.
+    #1.더하고 연산할지
+    #2.연산하고 더할지 
+    #판단하는 기준은 모든걸 훑을 수 있는지 봐야한다.
+    #1번이 안되는 이유는 더하는 순간 0인 지점은 내가 체크를 누락하게됨.
+    #앞으로 이런 상황이 생기면 꼭 다 훑기 가능한지 찬찬히 따져보자.
+    
     tmp-=target[point_front]
     point_front+=1
   else:
