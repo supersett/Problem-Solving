61328b4fe090c7fb90eb9c1f959e94df15e9f92e
supersett <nolang3748@naver.com>
2023-02-20 10:48:18 +0900

    BOJ : 1475 구현

---
+++ 스터디/로드맵/part2/1475.py
@@ -0,0 +1,19 @@
+import sys,math
+from collections import defaultdict
+input=sys.stdin.readline
+
+count=0
+count_6or9=0
+num=input().rstrip()
+d=defaultdict(int)
+
+for i in range(0,10):
+  d[i]=0
+
+#print(d)
+
+for i in num:
+  d[int(i)]+=1
+
+max_cnt=max(d[1],d[2],d[3],d[4],d[5],d[7],d[8],(d[6]+d[9]+1)//2)
+print(max_cnt)
